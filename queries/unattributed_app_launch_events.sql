3be903306eddbd16ec0b6558679a783e924e5a1a
Jeff Oberlander <joberlander@curiouslearning.org>
2025-01-28 09:13:31 -0800

    Refactored unattributed query

--- queries/unattributed_app_launch_events.sql
+++ queries/unattributed_app_launch_events.sql
@@ -7,7 +7,7 @@ WITH
       cr_user_id_params.value.string_value AS cr_user_id,
       ROW_NUMBER() OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp ASC) AS event_rank
     FROM
-      `ftm-b9d99.analytics_159643920.events_20*` AS A,
+      `ftm-b9d99.analytics_159643920.events_202*` AS A,
       UNNEST(event_params) AS cr_user_id_params,
       UNNEST(user_properties) AS campaign_props,
       UNNEST(user_properties) AS source_props
@@ -19,48 +19,47 @@ WITH
       AND campaign_props.value.string_value IS NOT NULL
       AND source_props.key = 'source'
       AND source_props.value.string_value IS NOT NULL
-      AND CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) BETWEEN '2024-11-08'
-      AND CURRENT_DATE() )
+        AND CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) BETWEEN '2024-11-08' AND CURRENT_DATE()
+ )
   WHERE
     event_rank = 1 ),
   query2_ranked_events AS (
   SELECT
     user_pseudo_id,
     event_date,
-    cr_user_id_params.value.string_value AS cr_user_id,
+    (SELECT crid.value.string_value 
+       FROM UNNEST(event_params) AS crid 
+       WHERE crid.key = 'cr_user_id') AS cr_user_id,
     geo.country AS country,
     geo.city AS city,
     geo.region AS region,
-    traffic_source.name AS traffic_source,
-    LOWER(REGEXP_EXTRACT(language_params.value.string_value, '[?&]cr_lang=([^&]+)')) AS app_language,
+    traffic_source.name AS traffic_source_name,
+    traffic_source.source AS traffic_source_source,
+    LOWER((
+
+        SELECT LOWER(REGEXP_EXTRACT(lp.value.string_value, '[?&]cr_lang=([^&]+)'))
+        FROM UNNEST(event_params) AS lp
+        WHERE lp.key = 'web_app_url'
+    )) AS app_language,
     CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) AS first_open,
     ROW_NUMBER() OVER (PARTITION BY user_pseudo_id ORDER BY event_timestamp ASC) AS event_rank
   FROM
-    `ftm-b9d99.analytics_159643920.events_20*` AS A
-  LEFT JOIN
-    UNNEST(event_params) AS language_params
-  ON
-    language_params.key = 'web_app_url'
-  LEFT JOIN
-    UNNEST(event_params) AS cr_user_id_params
-  ON
-    cr_user_id_params.key = 'cr_user_id'
-  LEFT JOIN
-    UNNEST(user_properties) AS campaign_props
-  ON
-    campaign_props.key = 'campaign_id'
-  LEFT JOIN
-    UNNEST(user_properties) AS source_props
-  ON
-    source_props.key = 'source'
+    `ftm-b9d99.analytics_159643920.events_202*`
   WHERE
     app_info.id = 'org.curiouslearning.container'
-    AND language_params.value.string_value LIKE 'https://feedthemonster.curiouscontent.org%'
     AND event_name = 'app_launch'
-    AND CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) BETWEEN '2024-11-08'
-    AND CURRENT_DATE()
-    AND campaign_props.key IS NULL
-    AND source_props.key IS NULL )
+    AND JSON_EXTRACT(
+          (SELECT up.value.string_value 
+           FROM UNNEST(user_properties) AS up 
+           WHERE up.key = 'campaign_id'),
+          '$'
+        ) IS  NULL
+    AND CAST(DATE(TIMESTAMP_MICROS(user_first_touch_timestamp)) AS DATE) BETWEEN '2024-11-08' AND CURRENT_DATE()
+
+    AND (SELECT lp.value.string_value 
+         FROM UNNEST(event_params) AS lp 
+         WHERE lp.key = 'web_app_url') LIKE 'https://feedthemonster.curiouscontent.org%' )
+
 SELECT
   user_pseudo_id,
   event_date,
@@ -68,7 +67,8 @@ SELECT
   country,
   city,
   region,
-  traffic_source,
+  traffic_source_name,
+  traffic_source_source,
   app_language,
   first_open
 FROM
